363e34effc6132f3575f66d621cc2f93e8e58420
Nelhe <nel.herve@gmail.com>
2024-10-02 16:44:31 +0200

    Fini l'analyse multivariée

--- Accidents_de_vélo/ui.R
+++ Accidents_de_vélo/ui.R
@@ -15,7 +15,7 @@ library(plotly)
 load("don.RData")
 var_analyse <- names(don)[c(2:5,10:18,20,21)]
 
-# Define UI for application that draws a histogram
+
 navbarPage(title = "Accidents de vélo en France",
            # 1er onglet description ============================================
            tabPanel(title = "Description",
@@ -30,11 +30,11 @@ navbarPage(title = "Accidents de vélo en France",
                                         wellPanel("Paramètres",
                                                   style = "background : skyblue"),
                                         actionButton('parametres11','Valider')
-                                        ),
+                                 ),
                                  column(width = 8,
                                         leafletOutput("leaflet")
-                                        )
-                                 ),
+                                 )
+                        ),
                         
                         # Graphiques de base -- -- -- -- -- -- -- -- -- -- -- --
                         tabPanel("Graphiques",
@@ -42,10 +42,10 @@ navbarPage(title = "Accidents de vélo en France",
                                         wellPanel("Paramètres",
                                                   style = "background : lightgreen"),
                                         actionButton('parametres12','Valider')
-                                        ),
-                                 column(width = 8
-                                        )
                                  ),
+                                 column(width = 8
+                                 )
+                        ),
                         
                         # Table de données -- -- -- -- -- -- -- -- -- -- -- -- 
                         tabPanel("Données",
@@ -53,47 +53,98 @@ navbarPage(title = "Accidents de vélo en France",
                                         wellPanel("Paramètres",
                                                   style = "background : khaki"),
                                         actionButton('parametres13','Valider')
-                                        ),
+                                 ),
                                  column(width = 8,
                                         DTOutput("donnees")
-                                        )
                                  )
-                             ))
-                    ),
+                        )
+                      ))
+           ),
            # 2ème onglet analyse stats =========================================
            navbarMenu("Analyse",
                       tabPanel("Analyse univariée",
-                               fluidRow(column(3,
-                                               selectInput('varuni',
-                                                           label = 'Variable à représenter',
-                                                           choices = setNames(var_analyse,
-                                                                              c("Année", "Mois", "Jour", "Heure",
-                                                                                "Agglomération", "Luminosité", "Météo", 
-                                                                                "Type de route", "Type de surface", 
-                                                                                "Gravité", "Sexe", "Âge", "Type de trajet", 
-                                                                                "Port de casque", "Port de gilet"))),
-                                               sliderInput('annee',
-                                                           label = 'Plage temporelle',
-                                                           min = 2005,
-                                                           max = 2021,
-                                                           value = c(2005, 2021),
-                                                           sep = '')),
-                                        column(9,
-                                               plotlyOutput('graph_uni')))),
-                
+                               fluidPage(
+                                 h2("Analyse univariée"),
+                                 p("Représentez le nombre d'accidents en fonction d'une des variables de la liste à gauche ci-dessous."),
+                                 br(),
+                                 p("Vous pouvez également choisir la plage temporelle."),
+                                 p(em("Remarque : les incidents nocturnes, de 0:00 à 9:00, n'ont été enregistrés que depuis 2019.")),
+                                 br(),
+                                 hr(),
+                                 
+                                 fluidRow(column(3,
+                                                 selectInput('varuni',
+                                                             label = 'Variable à représenter',
+                                                             choices = setNames(var_analyse,
+                                                                                c("Année", "Mois", "Jour", "Heure",
+                                                                                  "Agglomération", "Luminosité", "Météo", 
+                                                                                  "Type de route", "Type de surface", 
+                                                                                  "Gravité", "Sexe", "Âge", "Type de trajet", 
+                                                                                  "Port de casque", "Port de gilet"))),
+                                                 sliderInput('annee',
+                                                             label = 'Plage temporelle',
+                                                             min = 2005,
+                                                             max = 2021,
+                                                             value = c(2005, 2021),
+                                                             sep = '')),
+                                          column(9,
+                                                 plotlyOutput('graph_uni'))))),
+                      
                       tabPanel("Analyse multivariée",
-                               fluidRow(column(3,
-                                               selectInput('varmulti',
-                                                           label = 'Variable à représenter',
-                                                           choices = setNames(var_analyse[-c(5,10,11,14:15)],
-                                                                              c("Année", "Mois", "Jour", "Heure",
-                                                                                "Luminosité", "Météo", "Type de route", 
-                                                                                "Type de surface", "Âge", "Type de trajet"))),
-                                               "Remarque : les variables à 2 modalités ont été retirées."),
-                                        column(9,
-                                               plotOutput('plot_CA'))))
-                      ),
+                               fluidPage(
+                                 h2("Analyse multivariée"),
+                                 p( HTML("Deux types d'analyses sont disponibles : 
+                                   <ul> <li> une ACM (Analyse des Correspondances Multiples) sur les variables de votre choix ; </li>
+                                   <li> une AC (Analyse des Correspondances) mettant en relation la gravité d'un accident et une variable de votre choix. </li>
+                                   </ul> ")),
+                                 p(em("Remarque : la construction de l'ACM peut prendre plusieurs secondes.")),
+                                 hr(),
+                                 
+                                 tabsetPanel(
+                                   tabPanel("ACM",
+                                            br(),
+                                            fluidRow(
+                                              column(2,
+                                                     checkboxGroupInput("var_acm",
+                                                                        label = "Variables actives :",
+                                                                        choices = setNames(var_analyse[- which(var_analyse == "grav")],
+                                                                                           c("Année", "Mois", "Jour", "Heure",
+                                                                                             "Agglomération", "Luminosité", "Météo", 
+                                                                                             "Type de route", "Type de surface", 
+                                                                                             "Sexe", "Âge", "Type de trajet", 
+                                                                                             "Port de casque", "Port de gilet")),
+                                                                        selected = var_analyse[c(5:9, 13:15)]),
+                                                     actionButton("constr_acm", 
+                                                                  "Construire l'ACM"),
+                                                     br()),
+                                              
+                                              column(5,
+                                                     plotOutput("plot_MCA_ind", width = '600px', height = '400px')),
+                                              
+                                              column(5,
+                                                     plotOutput("plot_MCA_var", width = '600px', height = '400px'))
+                                            )),
+                                   
+                                   tabPanel("AC",
+                                            br(),
+                                            fluidRow(column(2,
+                                                            selectInput('var_ac',
+                                                                        label = 'Variable à représenter',
+                                                                        choices = setNames(var_analyse[-c(5,10,11,14:15)],
+                                                                                           c("Année", "Mois", "Jour", "Heure",
+                                                                                             "Luminosité", "Météo", "Type de route", 
+                                                                                             "Type de surface", "Âge", "Type de trajet"))),
+                                                            p(em("Remarque : les variables à 2 modalités ont été retirées."))),
+                                                     
+                                                     column(10,
+                                                            plotOutput('plot_CA'))
+                                            )
+                                   )
+                                 )
+                               )
+                      )
+           ),
            # 3ème onglet analyse spatio-temp ===================================
            tabPanel("Analyse spatio-temporelle"
-                    )
            )
+)
