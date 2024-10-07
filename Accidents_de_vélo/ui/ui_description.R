c40755a4798ce5e9142748559c175b2259e3f12e
leciet <leslie.cieters@gmail.com>
2024-10-07 16:00:02 +0200

    modification texte descriptif onglet 1

--- Accidents_de_vélo/ui/ui_description.R
+++ Accidents_de_vélo/ui/ui_description.R
@@ -2,8 +2,8 @@ tabPanel(title = "Description",
          HTML("<h1> Contexte </h1>
                           <body> Selon l'Observatoire national interministériel de la sécurité routière (ONISR), le risque pour un cycliste d’être victime d’un accident est <b>trois fois plus élevé</b> que pour un automobiliste. Le risque d’être gravement blessé est quant à lui <b>seize fois plus élevé</b>.<br/>
                           Pour lutter contre ce phénomène, le gouvernement a mis en place plusieurs règles de sécurité routière.Ainsi, le port d'un gilet réfléchissant et la présence d'un système d'éclairage et sonore est obligatoire et le port du casque est vivement recommandé. 
-                          A ces mesures s'ajoute le développement de pistes cyclabes dans les agglomérations françaises et de nombreuses campagnes sur la sécurité routière.</body><br/>
-                          <h4>Ces mesures sont-elles suffisantes pour réduire le nombre d'accidents impliquant des cyclistes, ainsi que leur gravité, en France ?</h4>
+                          A ces mesures s'ajoute le développement de pistes cyclabes dans les agglomérations françaises et de nombreuses campagnes sur la sécurité routière. En 2018 notamment, la vitesse sur les routes départementales a été diminué à 80km/h. Mais ces mesures sont-elles suffisantes ?</body><br/>
+                          <h4>Observe-t-on un changement dans le nombre et la gravité des accidents et/ou de l'usage d'équipement ces dernières années ?</h4>
                           <hr>"),
          fluidRow(
            tabsetPanel(
@@ -19,7 +19,10 @@ tabPanel(title = "Description",
                                        # choix de la gravité à afficher
                                        selectInput(inputId = "gravMap1",
                                                    label = "Gravité de l'accident *",
-                                                   selected = "Indemne",
+                                                   selected = c("Indemne", 
+                                                                "Blessé léger",
+                                                                "Blessé hospitalisé",
+                                                                "Tué"),
                                                    choices = c("Indemne", 
                                                                "Blessé léger",
                                                                "Blessé hospitalisé",
